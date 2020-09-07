955d66348f283d35ca48b132e6b27e2e59848515
Omar <omarmuhammed1998.om@gmail.com>
2020-09-07 23:25:33 +0200

    Applied the __main__ pattern in todolist.py

--- todolist.py
+++ todolist.py
@@ -2,8 +2,9 @@ from datetime import datetime, timedelta
 import sys
 from functions import *
 
-while True:
-    entry = input('''
+def main():
+    while True:
+        entry = input('''
 1) Today's tasks
 2) Week's tasks
 3) All tasks
@@ -13,35 +14,38 @@ while True:
 7) Move deadline
 0) Exit
 ''')
-    today = datetime.today()
-    # Check the entry in the menu
-    if entry == '1':
-        today_tasks(today)
+        today = datetime.today()
+        # Check the entry in the menu
+        if entry == '1':
+            today_tasks(today)
 
-    elif entry == '2':
-        week_tasks(today)
+        elif entry == '2':
+            week_tasks(today)
 
-    elif entry == '3':
-        all_tasks()
+        elif entry == '3':
+            all_tasks()
 
-    elif entry == '4':
-        missed_tasks(today)
+        elif entry == '4':
+            missed_tasks(today)
 
-    elif entry == '5':
-        add_task()
+        elif entry == '5':
+            add_task()
 
-    elif entry == '6':
-        delete_task()
+        elif entry == '6':
+            delete_task()
 
-    elif entry == '7':
-        move_deadline()
+        elif entry == '7':
+            move_deadline()
 
-    elif entry == '0':
-        print('Bye!')
-        session.close()
-        sys.exit(0)
+        elif entry == '0':
+            print('Bye!')
+            session.close()
+            sys.exit(0)
 
-    else:
-        print('Invalid entry!')
+        else:
+            print('Invalid entry!')
 
-session.close()
+    session.close()
+
+if __name__ == "__main__":
+    main()
